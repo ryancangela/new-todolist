b9a139335bcdaf2fea45816b3df0ce0af0559baf
Ryan Angela <ryan.c.angela@gmail.com>
2022-04-28 23:55:42 -0400

    more mobile & better empty state

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-'strict';
+"strict";
 
 // document.getElementById("demo").innerHTML = myFunction("John");
 // function myFunction(name) {
@@ -9,24 +9,23 @@
 //   document.querySelector(`.lock`).classList.toggle(`unlock`)
 // }
 
-  // function input(){
-  //   var input1 = document.getElementById("todo-input").value;
-  //   document.getElementById("output").value = input1;
-  //   }
+// function input(){
+//   var input1 = document.getElementById("todo-input").value;
+//   document.getElementById("output").value = input1;
+//   }
 
-
-  // To Do List - Adding & removing tasks
-  function inputValue() {
-    const task = document.createElement("p");
-    const inputValue = document.getElementById("todo-input").value;
-    const t = document.createTextNode(inputValue);
-    task.appendChild(t);
-    if (inputValue === ``) {
-      alert(`You can't just do nothing...`);
+// To Do List - Adding & removing tasks
+function inputValue() {
+  const task = document.createElement("p");
+  const inputValue = document.getElementById("todo-input").value;
+  const t = document.createTextNode(inputValue);
+  task.appendChild(t);
+  if (inputValue === ``) {
+    alert(`You can't just do nothing...`);
   } else {
     document.getElementById("outputList").appendChild(task);
   }
-  document.getElementById("todo-input").value= "";
+  document.getElementById("todo-input").value = "";
 
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
@@ -35,22 +34,17 @@
   task.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       let div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
 let close = document.getElementsByClassName("close");
 let i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     let div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
-
-// {
-//   element.classList.toggle("dark-mode");
-// }
-
